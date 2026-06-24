da4730e2fe63a69c7b8a21bed6e8723c998076ef
François de Saint Palais <francois.desaintpalais@master-developpement-logiciel.fr>
2026-06-25 01:11:44 +0200

    feat: utilisation du version catalog
    sauf pour une dépendance, mais je ne sais pas comment utiliser les
    classifier

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
     application
-    kotlin("jvm") version "2.4.0"
-    kotlin("plugin.spring") version "2.1.20"
-    id("org.openjfx.javafxplugin") version "0.1.0"
-    id("org.springframework.boot") version "4.0.6"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.javafx)
+    alias(libs.plugins.spring.boot)
 }
 
 group = "com.fsp"
@@ -12,12 +12,9 @@ version = "1.1.1"
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://jitpack.io")  }
+    maven { url = uri("https://jitpack.io") }
     mavenLocal()
 }
-
-val junitVersion = "6.0.3"
-
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
@@ -26,6 +23,7 @@ tasks.withType<JavaCompile> {
 application {
     mainClass.set("com.fsp.plantapp.Launcher")
 }
+
 kotlin {
     jvmToolchain(25)
 }
@@ -36,14 +34,14 @@ javafx {
 }
 
 dependencies {
-    implementation("io.github.francois389:javaspringfx:0.2.1")
-    implementation("net.sourceforge.plantuml:plantuml:1.2026.0")
-    implementation("com.github.hervegirod:fxsvgimage:1.7.3")
+    implementation(libs.javaspringfx)
+    implementation(libs.plantuml)
+    implementation(libs.fxsvgimage)
     implementation("com.github.hervegirod:fxsvgimage:1.7.3:cssparser")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.junit.platform:junit-platform-launcher:$junitVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.platform.launcher)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
 tasks.withType<Test> {
