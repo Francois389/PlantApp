99b5b16ac205b5059b9f7c56f66c7d678270d289
François de Saint Palais <francois.desaintpalais@master-developpement-logiciel.fr>
2026-06-27 13:29:12 +0200

    feat: plugin pour créer un installateur .deb

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,21 @@ plugins {
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.javafx)
     alias(libs.plugins.spring.boot)
+    alias(libs.plugins.badass.runtime)
 }
 
+fun getLastGitTag(): String {
+    return Runtime.getRuntime()
+        .exec(arrayOf("git", "tag", "--sort=-version:refname"))
+        .inputStream
+        .bufferedReader()
+        .readLine()
+        ?.removePrefix("v")
+        ?: System.getenv("PLANTAPP_VERSION")
+        ?: throw RuntimeException("No git tags found")
+}
+version = getLastGitTag()
 group = "com.fsp"
-version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -46,4 +57,45 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.named("jpackageImage") {
+    dependsOn("runtime")
+}
+
+runtime {
+    imageZip.set(project.file("${project.layout.buildDirectory.get()}/image.zip"))
+
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+
+    // Modules nécessaires pour Spring Boot + JavaFX
+    modules.set(
+        listOf(
+            "java.base", "java.desktop", "java.logging", "java.management",
+            "java.naming", "java.net.http", "java.sql", "java.xml",
+            "jdk.unsupported", "jdk.crypto.ec"
+        )
+    )
+
+    jpackage {
+        imageName = "PlantApp"
+        skipInstaller = false
+        installerName = "PlantApp"
+        appVersion = project.version.toString()
+
+        // vendor = "fsp"
+        // Icône (chemins différents selon l'OS)
+        imageOptions = listOf("--icon", "src/main/resources/PlantApp_Logo.png")
+
+        targetPlatformName = "current" // build pour l'OS courant
+
+        installerOptions = listOf(
+            "--description", "PlantUML diagram editor",
+            "--vendor", "fsp",
+            "--linux-shortcut",
+            "--linux-package-name", "plantapp",
+            "--linux-app-category", "Development",
+            "--resource-dir", "packaging/linux",
+        )
+    }
 }
